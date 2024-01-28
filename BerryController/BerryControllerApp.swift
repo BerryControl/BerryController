12eb84a4a637d72c342c764804b37de59da70d4f
Thomas Bonk <thomas@meandmymac.de>
2024-01-28 19:48:51 +0100

    Initial Commit

---
+++ BerryController/BerryControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryControllerApp.swift
+//  BerryController
+//
+//  Created by Thomas Bonk on 28.01.24.
+//
+
+import SwiftUI
+
+@main
+struct BerryControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerryController/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BerryController
+//
+//  Created by Thomas Bonk on 28.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
